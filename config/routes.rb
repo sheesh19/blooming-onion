05f7aee6caae017583a3dc1b343bcd79a1bc72c6
Sheila Leveille <sbleveille@gmail.com>
2020-12-05 15:36:26 -0500

    heroku taggable

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index, :show, :new, :create ] do
+    resources :product_reviews, only: [ :create ]
+  end
+  
+  resources :ingredients, only: [ :index, :show, :create ] do
+    resources :ingredient_reviews, only: [ :create ]
+  end
 end
